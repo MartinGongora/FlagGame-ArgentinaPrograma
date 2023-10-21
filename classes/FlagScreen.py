736d505ceeca41cc808990c26eef7d06a8d2e081
Martin Gongora <martin_gongora@hotmail.com>
2023-10-20 23:42:31 -0300

    Agrego sumatoria de puntos, y ciclo de banderas

--- classes/FlagScreen.py
+++ classes/FlagScreen.py
@@ -9,10 +9,11 @@ class FlagScreen(Screen):
     timer_label = StringProperty()
     country_name = StringProperty()
     country_flag = StringProperty()
+    answer = StringProperty()
 
     def on_enter(self):
         self.time_left = 60
-
+        self.score = 0
         self.game_cycle() 
         self.start_timer() 
         
@@ -20,8 +21,15 @@ class FlagScreen(Screen):
     def game_cycle(self): 
         self.country = random.choice(MDApp.get_running_app().lista_paises)
         self.country_name = self.country.get("translations").get("spa").get("common")
-        print(self.country_name)
         self.country_flag = self.country.get("flags").get("png")
+        self.correct_answer = self.country_name
+    
+    def check_answer(self, instance):
+        # Se verifica si el boton presionado contiene la respuesta correcta
+        if instance.text == self.correct_answer:
+            print("Acierto!")
+            self.score += 1    
+        self.game_cycle()   
 
     def start_timer(self):
         self.evento_reloj = Clock.schedule_interval(self.update_timer, 1)
@@ -31,8 +39,7 @@ class FlagScreen(Screen):
         self.time_left -= 1
         if self.time_left < 0:
             print("Termino el tiempo")
-            self.evento_reloj.cancel()
-            self.manager.current = 'result'
+            self.go_to_question_screen()
         else:
             self.timer_label = '{:02d}:{:02d}'.format(*divmod(self.time_left, 60))
 
