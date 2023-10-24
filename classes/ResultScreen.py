7c9d1eb355d92cbe2b0da7e119260d45872a5c42
Martin Gongora <martin_gongora@hotmail.com>
2023-10-24 16:55:58 -0300

    Elimino prints

--- classes/ResultScreen.py
+++ classes/ResultScreen.py
@@ -10,12 +10,12 @@ class ResultScreen(Screen):
     score = StringProperty()
 
     def on_enter(self):
-        print(MDApp.get_running_app().score)
-        print(MDApp.get_running_app().list_answers)
         self.list_answers = MDApp.get_running_app().list_answers
         self.score = MDApp.get_running_app().score
+
         results_list = self.ids.results_list
-        results_list.clear_widgets() 
+        results_list.clear_widgets()
+         
         for answer in self.list_answers:
             country_name = answer.get("country")
             result = answer.get("valid")
