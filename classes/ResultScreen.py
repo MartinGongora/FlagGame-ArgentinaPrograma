cb99ad3da0b90148f8d80d24768f90499a11cb17
Martin Gongora <martin_gongora@hotmail.com>
2023-10-24 16:53:15 -0300

    Agrego vista de resultados

--- classes/ResultScreen.py
+++ classes/ResultScreen.py
@@ -1,6 +1,10 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
 from kivy.properties import StringProperty
+from kivymd.uix.list import OneLineAvatarListItem
+
+class CountryItem(OneLineAvatarListItem):
+    pass
 
 class ResultScreen(Screen):
     score = StringProperty()
@@ -8,4 +12,13 @@ class ResultScreen(Screen):
     def on_enter(self):
         print(MDApp.get_running_app().score)
         print(MDApp.get_running_app().list_answers)
+        self.list_answers = MDApp.get_running_app().list_answers
         self.score = MDApp.get_running_app().score
+        results_list = self.ids.results_list
+        results_list.clear_widgets() 
+        for answer in self.list_answers:
+            country_name = answer.get("country")
+            result = answer.get("valid")
+            country_item = CountryItem(text=f"{country_name} - Correcto?: {result}")
+
+            results_list.add_widget(country_item)
